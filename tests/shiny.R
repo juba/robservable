14f79dd0098fc5660808335991b5168f1c6d1bde
Julien <julien@nozav.org>
2020-06-11 17:05:53 +0200

    Add tests files

---
+++ tests/shiny.R
@@ -0,0 +1,58 @@
+library(shiny)
+
+library(gapminder)
+library(tidyverse)
+data(gapminder)
+dates <- sort(unique(gapminder$year))
+dates <- to_js_date(as.Date(as.character(dates), format = "%Y"))
+
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Test robservable"),
+
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(
+                "var",
+                label = "Variable",
+                choices = c("lifeExp", "pop", "gdpPercap")
+            )
+        ),
+
+        mainPanel(
+           robservableOutput("chart")
+        )
+    )
+)
+
+
+server <- function(input, output) {
+
+    series <- reactive({
+        purrr::map(unique(gapminder$country), ~{
+            name <- .x
+            var <- sym(input$var)
+            values <- gapminder %>% filter(country == .x) %>% pull(!!var)
+            list(name = name, values = values)
+        })
+    })
+
+
+    output$chart <- renderRobservable({
+        df <- list(
+            y = input$var,
+            series = series(),
+            dates = dates
+        )
+
+        robservable(
+            "@d3/multi-line-chart",
+            cell = "chart",
+            input = list(data = df),
+            width = 800
+        )
+    })
+}
+
+shinyApp(ui = ui, server = server)
