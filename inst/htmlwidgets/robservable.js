a2fd52cacbc65c54e4b5f5fc4fac2dafac083522
Julien <julien@nozav.org>
2020-06-11 14:40:07 +0200

    Remove input_df and override styles when rendering whole notebook

--- inst/htmlwidgets/robservable.js
+++ inst/htmlwidgets/robservable.js
@@ -18,24 +18,20 @@ HTMLWidgets.widget({
                     let notebook = nb.default;
 
                     let output;
-                    if (x.output_cell === null) {
+                    if (x.cell === null) {
                         output = observablehq.Inspector.into(document.body);
+                        document.getElementById('htmlwidget_container').style["display"] = "none";
+                        document.querySelector('body').style["overflow"] = "auto";
                     }
-                    if (typeof x.output_cell == "string") {
+                    if (typeof x.cell == "string") {
                         output = name => {
-                            if (name === x.output_cell) {
+                            if (name === x.cell) {
                                 return new observablehq.Inspector(el);
                             }
                         }
                     }
  
                     const main = runtime.module(notebook, output);
-
-                    const df_inputs = x.input_df === null ? {} : x.input_df;
-                    Object.entries(df_inputs).forEach(([key, value]) => {
-                        value = HTMLWidgets.dataframeToD3(value);
-                        main.redefine(key, value);
-                    })
 
                     const inputs = x.input === null ? {} : x.input;
                     Object.entries(inputs).forEach(([key, value]) => {
