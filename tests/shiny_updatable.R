394e098aab68d911fef3d9048d01222577a90627
Julien <julien@nozav.org>
2020-06-12 16:35:56 +0200

    Add updatable shiny app example

---
+++ tests/shiny_updatable.R
@@ -0,0 +1,44 @@
+library(shiny)
+library(robservable)
+
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Test robservable"),
+
+    sidebarLayout(
+        sidebarPanel(
+            shiny::actionButton("update", "Update")
+        ),
+
+        mainPanel(
+           robservableOutput("chart")
+        )
+    )
+)
+
+
+server <- function(input, output) {
+
+    data <- reactive({
+        input$update
+        data.frame(
+            name = LETTERS[1:10],
+            value = round(runif(10)*100)
+        )
+    })
+
+    output$chart <- renderRobservable({
+
+        robservable(
+            "@juba/updatable-bar-chart",
+            cell = c("chart", "draw"),
+            hide = "draw",
+            input = list(
+                data = data()
+            )
+        )
+    })
+}
+
+shinyApp(ui = ui, server = server)
