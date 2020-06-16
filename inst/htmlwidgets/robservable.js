c3b7d7de53a48297b792efa553990560192b2a81
Julien <julien@nozav.org>
2020-06-16 14:54:31 +0200

    Refactor robservable.js with a class declaration

--- inst/htmlwidgets/robservable.js
+++ inst/htmlwidgets/robservable.js
@@ -1,54 +1,90 @@
-<<<<<<< HEAD
 class RObservable {
 
-    // Instance fields
-    params
-
-    // Constructor
-    async constructor(el, params) {
-
-        this.el = el;
-        this.params = params;
-
-        // Load Runtime by overriding width stdlib method if fixed width is provided
-        const stdlib = new observablehq.Library;
-        let library;
-        let runtime;
-        if (params.robservable_width !== undefined) {
-            library = Object.assign(stdlib, { width: params.robservable_width });
-            runtime = new observablehq.Runtime(library);
-        } else {
-            runtime = new observablehq.Runtime();
+    // Pattern taken from https://stackoverflow.com/a/43433773
+    constructor(el, params, notebook) {
+        // Throw error if notebook module has not been loaded asynchronously
+        if (typeof notebook === 'undefined') {
+            throw new Error('Cannot be called directly');
         }
 
-        // Dynamically import notebook module
+        this.el = el;
+        this._params = params;
+
+        let runtime = this.build_runtime();
+        let inspector = this.build_inspector();
+        this.main = runtime.module(notebook, inspector);
+
+    }
+
+    // async builder to dynamically import notebook module
+    static async build(el, params) {
         const url = `https://api.observablehq.com/${params.notebook}.js?v=3`;
         let nb = await import(url);
-        let notebook = nb.default;
-        let output;
+        return new RObservable(el, params, nb.default);
+    }
 
-        if (params.cell !== null) {
+    // Build observable runtime
+    build_runtime() {
+        // Load Runtime by overriding width stdlib method if fixed width is provided
+        let library = new observablehq.Library;
+        if (this.params.robservable_width !== undefined) {
+            library = Object.assign(library, { width: this.params.robservable_width });
+        }
+        return new observablehq.Runtime(library);
+    }
+
+    // Build Observable inspector
+    build_inspector() {
+        if (this.params.cell !== null) {
             // If output is one or several cells
-            let output_divs = create_output_divs(el, params.cell, params.hide);
+            let output_divs = this.create_output_divs(this.el, this.params);
 
-            output = (name) => {
-                if (params.cell.includes(name)) {
+            return (name) => {
+                if (this.params.cell.includes(name)) {
                     return new observablehq.Inspector(output_divs.get(name));
                 }
             }
         } else {
             // If output is the whole notebook
-            output = observablehq.Inspector.into(el);
+            return observablehq.Inspector.into(this.el);
         }
-
-        // Run main
-        this.main = runtime.module(notebook, output);
-
     }
+
+    // params getter
+    get params() {
+        return this._params;
+    }
+
+    // params setter
+    set params(value) {
+        this._params = value;
+        // Add observers and update variables
+        this.add_variable_observers();
+        this.update_variables();
+    }
+
+    // Create the <div> elements for each cell to render
+    create_output_divs() {
+
+        const cell = !Array.isArray(this.params.cell) ? [this.params.cell] : this.params.cell;
+        const hide = !Array.isArray(this.params.hide) ? [this.params.hide] : this.params.hide;
+
+        let output_divs = new Map();
+        cell.forEach(name => {
+            let div = document.createElement("div");
+            div.className = css_safe(name);
+            // hide cell if its name is in params.hide
+            if (hide.includes(name)) div.style["display"] = "none";
+            this.el.appendChild(div);
+            output_divs.set(name, div);
+        })
+        return output_divs;
+    }
+
 
     // Add an observer on a notebook variable that sync its value to a Shiny input
     add_observer(name, variable) {
-        this.module.variable({
+        this.main.variable({
             fulfilled(value, name) {
                 console.log(value, name)
                 if (HTMLWidgets.shinyMode) {
@@ -59,61 +95,42 @@ class RObservable {
                 }
             }
         })
-            .define(el.id + '_robservable_' + name, [variable], x => x);
+            // el.id must be added to support Shiny modules
+            // '_robservable_' is added to avoid name conflicts with notebook variables
+            .define(this.el.id + '_robservable_' + name, [variable], x => x);
     }
 
-    // Observers setter
-    set observers(observers) {
-
-        // If in Shiny mode and observers are set then set these up in Observable
+    // Add observers from params.observers to cells
+    add_variable_observers() {
+        let observers = this.params.observers;
         if (observers !== null) {
             // if only one observer and string then force to array
             observers = !Array.isArray(observers) && typeof (observers) === "string" ? [observers] : observers;
             // If source is an R character vector
             if (Array.isArray(observers)) {
-                observers.forEach((d) => add_observer(d, d));
+                observers.forEach((d) => this.add_observer(d, d));
             }
-            // If source is an R named list
+            // If source is an  R named list
             if (!Array.isArray(observers) && typeof (observers) === "object") {
-                Object.entries(observers).forEach(([key, value]) => add_observer(key, value));
+                Object.entries(observers).forEach(([key, value]) => this.add_observer(key, value));
             }
         }
 
-
     }
 
-    // Variables setter
-    set variables(inputs) {
-        inputs = inputs === null ? {} : inputs;
-        Object.entries(inputs).forEach(([key, value]) => {
-            this.module.redefine(key, value);
+    // Update notebook variables from params.input
+    update_variables() {
+        let input = this.params.input
+        input = input === null ? {} : input;
+        Object.entries(input).forEach(([key, value]) => {
+            this.main.redefine(key, value);
         })
     }
 
-
 }
-=======
->>>>>>> master
 
 
 
-// Create the <div> elements for each cell to render
-function create_output_divs(el, cell, hide) {
-
-    cell = !Array.isArray(cell) ? [cell] : cell;
-    hide = !Array.isArray(hide) ? [hide] : hide;
-
-    let output_divs = new Map();
-    cell.forEach(name => {
-        let div = document.createElement("div");
-        div.className = css_safe(name);
-        // hide cell if its name is in params.hide
-        if (hide.includes(name)) div.style["display"] = "none";
-        el.appendChild(div);
-        output_divs.set(name, div);
-    })
-    return output_divs;
-}
 
 
 HTMLWidgets.widget({
@@ -133,19 +150,19 @@ HTMLWidgets.widget({
 
             renderValue(params) {
 
+                // Check if module object already created
                 let module = el.module;
-                if (module === null || module.params.notebook !== params.notebook) {
-                    module = new RObservable(el, params);
-                    el.module = module;
+                if (module === undefined || module.params.notebook !== params.notebook) {
+                    // If not, create one
+                    RObservable.build(el, params).then(mod => {
+                        mod.params = params;
+                        el.module = mod;
+
+                    });
                 } else {
+                    // Else, update params
                     el.module.params = params;
                 }
-
-                // Add observers 
-                module.observers = params.observers;
-                // Update variables
-                module.variables = params.input;
-
 
             },
 
