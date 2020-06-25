34b1a757016f4ae152419db5bf9541bea6dd9670
Julien <julien@nozav.org>
2020-06-25 15:13:20 +0200

    On resize, override width and height if previous cell values come themselves from an override.
    Override must occur when update_width is true, and either input.width is undefined, or width_updated is true which means that current input.width comes from a previous override.
    
    Same for height.

--- inst/htmlwidgets/utils.js
+++ inst/htmlwidgets/utils.js
@@ -7,11 +7,13 @@ function css_safe(str) {
 // Update params height and width
 function update_height_width(params, height, width) {
     if (params.input === null) params.input = {};
-    if (params.input.width === undefined && params.update_width) {
+    if ((params.input.width === undefined || params.width_updated) && params.update_width) {
         params.input.width = width;
+        params.width_updated = true;
     }
-    if (params.input.height === undefined && params.update_height) {
+    if ((params.input.height === undefined || params.height_updated) && params.update_height) {
         params.input.height = height;
+        params.height_updated = true;
     }
     return params;
 }
