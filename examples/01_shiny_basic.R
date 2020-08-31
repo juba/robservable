834b3f8c44ca26613f7d9c11a3d3ed046b1ea853
Julien <julien@nozav.org>
2020-08-31 11:32:25 +0200

    Add basic shiny example

---
+++ examples/01_shiny_basic.R
@@ -0,0 +1,47 @@
+## Basic robservable Shiny app
+
+library(shiny)
+library(robservable)
+
+data("USPersonalExpenditure")
+
+ui <- fluidPage(
+    titlePanel("robservable basic Shiny app"),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(
+                "year",
+                label = "Year",
+                choices = colnames(USPersonalExpenditure)
+            )
+        ),
+        mainPanel(
+           robservableOutput("chart", width = 600)
+        )
+    )
+)
+
+
+server <- function(input, output) {
+
+    df <- reactive({
+        data.frame(
+            name = rownames(USPersonalExpenditure),
+            value = USPersonalExpenditure[, input$year]
+        )
+    })
+
+    output$chart <- renderRobservable({
+        robservable(
+            "@d3/horizontal-bar-chart",
+            include = "chart",
+            input = list(
+                data = df(),
+                margin = list(top = 20, right = 20, bottom = 30, left = 130)
+            ),
+            width = 600
+        )
+    })
+}
+
+shinyApp(ui = ui, server = server)
